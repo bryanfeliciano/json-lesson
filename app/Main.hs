a50b18250307cfcd4433e92f94bdc447607f1e1b
bryan <bryanfeliciano892@gmail.com>
2022-05-17 11:46:07 -0500

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Data.Aeson
+import Data.Text as T
+import Data.ByteString.Lazy as B 
+import Data.ByteString.Lazy.Char8 as BC
+import GHC.Generics
+
+import Lib
+
+main :: IO ()
+main = someFunc

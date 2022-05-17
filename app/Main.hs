fd00716cf78628ed6f4e7b91297ec8074d48f144
bryan <bryanfeliciano892@gmail.com>
2022-05-17 12:36:48 -0500

    data type,fromJSON and  toJSON added

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
+
 
 import Data.Aeson
 import Data.Text as T
@@ -8,5 +10,29 @@ import GHC.Generics
 
 import Lib
 
+-- testing encode to json -- 
+
+data Book = Book
+            {
+                tittle :: T.Text,
+                author :: T.Text,
+                year :: Int
+            } deriving (Show)
+
+instance FromJSON Book
+instance ToJSON Book
+
+
+myBook :: Book 
+myBook = Book
+         {
+             author = "Will Kurt",
+             tittle = "Learn Haskell",
+             year = 2017
+         }
+
+myBookJSON :: BC.ByteString
+myBookJSON = encode myBook         
+
 main :: IO ()
-main = someFunc
+main = print "hi"

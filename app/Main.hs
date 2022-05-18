967b0f75615fc21bddc94c369e44cc3445d10cd1
bryan <bryanfeliciano892@gmail.com>
2022-05-18 09:32:22 -0500

    All core features implemented

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import Data.Text as T
 import Data.ByteString.Lazy as B 
 import Data.ByteString.Lazy.Char8 as BC
 import GHC.Generics
+import Control.Monad
 
 import Lib
 
@@ -124,6 +125,13 @@ instance ToJSON ErrorMessage where
 
 anErrorMessage :: ErrorMessage
 anErrorMessage = ErrorMessage "Everything is Okay" 0
+
+printResults :: Maybe [NOAAResult] -> IO ()
+printResults Nothing = print "error loading data"
+printResults (Just results) = do
+   forM_ results (print . name)
+    print dataName 
+
 
 main :: IO ()
 main = print "hi"
